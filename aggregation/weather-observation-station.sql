156ce5733caf35e8c47f6be7c8172153614810b6
fildzaanf <93438625+fildzaanf@users.noreply.github.com>
2025-12-24 22:22:38 +0700

    update challenges

--- aggregation/weather-observation-station.sql
+++ aggregation/weather-observation-station.sql
@@ -25,3 +25,24 @@ FROM station
 WHERE lat_n > 38.7780
 ORDER BY lat_n ASC
 LIMIT 1;
+
+-- manhattan distance
+SELECT 
+  ROUND(
+    ABS(MIN(lat_n) - MAX(lat_n)) + 
+    ABS(MIN(long_w) - MAX(long_w)),
+    4
+  )
+FROM station;
+
+-- euclidean distance
+SELECT 
+  ROUND(
+    SQRT(
+      POWER(MAX(lat_n) - MIN(lat_n), 2) +
+      POWER(MAX(long_w) - MIN(long_w), 2)
+    ),
+    4
+  )
+FROM station;
+
